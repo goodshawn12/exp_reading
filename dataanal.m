dd75b3d57375d160b101ed7ff32d07f5642d16c3
margotwagner <mwagner92796@gmail.com>
2018-12-22 20:18:01 -0600

    Finished sorting event_markers. Still have to check right answers.

--- dataanal.m
+++ dataanal.m
@@ -3,7 +3,7 @@ clc;
 
 % Make sure eeglab is running with data imported 
 
-num_events = 85; % The number of trials being looked at (506 total)
+num_events = 506; % The number of trials being looked at (506 total)
 
 % Initialize arrays and counters
 type_array = zeros(num_events,1); % Array of 'types' of events, given my imported data (initialized)
@@ -18,13 +18,7 @@ i = 1;
 while i <= num_events % loop through all the data points
     type_array(i,:) = EEG.event(i).type; % event 'type' from data
     events_array(i,:) = extractBetween(type_array(i,:), "<ecode>","</ecode>"); % take out marker
-%     if events_array(i,:) == "cross"
-%         events_array(i,:) = [];
-%     elseif events_array(i,:) == "end_trial"
-%         events_array(i,:) = [];
-%     elseif events_array(i,:) == "break"
-%         events_array(i,:) = [];
-%     end
+   
     i = i + 1;
 end
 
@@ -33,6 +27,7 @@ start_i = find(events_array == "start") % Index where exp starts
 events_array = events_array(start_i+6:end); 
 assert(events_array(1) == "easy_5"); % Actual trials should start w. "easy_5"
 
+size(events_array,1)
 % Remove excess event markers
 i = 1; % Initialize counter
 while i <= size(events_array,1)
@@ -42,63 +37,68 @@ while i <= size(events_array,1)
         events_array(i,:) = [];
     elseif events_array(i,:) == "break"
         events_array(i,:) = [];
+    elseif events_array(i,:) == "6"
+        events_array(i,:) = [];
+    else
+        i = i + 1;
     end
-    i = i + 1;
+    %i = i + 1
+    %events_array
 end
 events_array
 
 act_num_events = size(events_array,1); % # of trials once xs event markers are removed
 
-% %------------------------------------------------------------------------
-% % Organize array by trial
-% 
-% % Initialize confidence (conf) and answer (ans) arrays
-% conf_array = [];
-% ans_array = [];
-% 
-% % Move confidence values to indep array
-% j = 1; % Initialize general index
-% while j <= act_num_events % loop thru trials
-%     if mod(j,3) == 0
-%         conf_array = [conf_array, events_array(j,:)];
-%         events_array(j,:) = "remove";
-%     end
-%     j = j + 1;
-% end
-% 
-% conf_array = conf_array';
-% 
-% % Fix general array
-% j = 1; % Initialize general index
-% while j <= size(events_array,1)
-%     if events_array(j,:) == "remove";
-%         events_array(j,:) = [];
-%     end
-%     j = j + 1;
-% end
-% 
-% % Move answers to indep array
-% j = 1; % Initialize general index
-% while j <=size(events_array,1)
-%     if mod(j,2) == 0
-%         ans_array = [ans_array, events_array(j,:)];
-%         events_array(j,:) = "remove";
-%     end
-%     j = j + 1;
-% end
-% 
-% ans_array = ans_array';
-% 
-% % Fix general array
-% j = 1; % Initialize general index
-% while j <= size(events_array,1)
-%     if events_array(j,:) == "remove";
-%         events_array(j,:) = [];
-%     end
-%     j = j + 1;
-% end
-% 
-% assert(size(events_array,1) == size(ans_array,1)); % Should be an answer for every trial
-% assert(size(events_array,1) == size(conf_array,1)); % Should be a confidence level for every trial
-% 
-% trial_array = [events_array, ans_array, conf_array]
+%------------------------------------------------------------------------
+% Organize array by trial
+
+% Initialize confidence (conf) and answer (ans) arrays
+conf_array = [];
+ans_array = [];
+
+% Move confidence values to indep array
+j = 1; % Initialize general index
+while j <= act_num_events % loop thru trials
+    if mod(j,3) == 0
+        conf_array = [conf_array, events_array(j,:)];
+        events_array(j,:) = "remove";
+    end
+    j = j + 1;
+end
+
+conf_array = conf_array';
+
+% Fix general array
+j = 1; % Initialize general index
+while j <= size(events_array,1)
+    if events_array(j,:) == "remove";
+        events_array(j,:) = [];
+    end
+    j = j + 1;
+end
+
+% Move answers to indep array
+j = 1; % Initialize general index
+while j <=size(events_array,1)
+    if mod(j,2) == 0
+        ans_array = [ans_array, events_array(j,:)];
+        events_array(j,:) = "remove";
+    end
+    j = j + 1;
+end
+
+ans_array = ans_array';
+
+% Fix general array
+j = 1; % Initialize general index
+while j <= size(events_array,1)
+    if events_array(j,:) == "remove";
+        events_array(j,:) = [];
+    end
+    j = j + 1;
+end
+
+assert(size(events_array,1) == size(ans_array,1)); % Should be an answer for every trial
+assert(size(events_array,1) == size(conf_array,1)); % Should be a confidence level for every trial
+
+trial_array = [events_array, ans_array, conf_array]
